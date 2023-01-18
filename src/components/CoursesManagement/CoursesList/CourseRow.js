990a2998b474ffe9324cc0e0b46e82980f824a09
Adrian González <adrigondo.profesional@gmail.com>
2023-01-18 17:27:50 -0600

    Add: binding to course row inputs

--- src/components/CoursesManagement/CoursesList/CourseRow.js
+++ src/components/CoursesManagement/CoursesList/CourseRow.js
@@ -1,39 +1,63 @@
 import React from "react";
+import "./CourseRow.css";
 
 export default class CourseRow extends React.Component{
+    handleInputText(type,event){
+        if(type==="color"){
+            this.props.onChangeColor(event.target.value);
+        }
+        else if(type==="name"){
+            this.props.onChangeName(event.target.value);
+        }
+        else if(type==="abbreviation"){
+            this.props.onChangeAbbreviation(event.target.value);
+        }
+        else if(type==="professor"){
+            this.props.onChangeProfessor(event.target.value);
+        }
+        // else if(type==="abbreviation"){
+            
+        // }
+    }
     render(){
         return(
             <div className="course-row">
                 <div className="course-row__color">
-                    <input type="color" />
+                    <input type="color"
+                        onChange={(event)=>{(this.handleInputText("color",event))}}
+                        value={ this.props.color }
+                    />
                 </div>
                 <div className="course-row__name">
                     <input type="text"
+                        onChange={(event)=>{(this.handleInputText("name",event))}}
                         value={ this.props.name }
                     />
                 </div>
                 <div className="course-row__abbreviation">
                     <input type="text"
+                        onChange={(event)=>{(this.handleInputText("abbreviation",event))}}
                         value={ this.props.abbreviation }
                     />
                 </div>
                 {
-                    this.props.professor===null?
-                        <div className="course-row__professor--button">
-                            <button
-                                onClick={()=>{
-                                    this.props.onClickOnShowProfessor()
-                                }}
-                            >
-                                Add professor
-                            </button>
-                        </div>
-                    :
-                        <div className="course-row__professor">
-                            <input type="text"
-                                value={ this.props.professor }
-                            />
-                        </div>
+                this.props.hasProfessor===false?
+                    <div className="course-row__professor--button">
+                        <button
+                            onClick={()=>{
+                                this.props.onClickOnShowProfessor()
+                            }}
+                        >
+                            Add professor
+                        </button>
+                    </div>
+                :
+                    <div className="course-row__professor">
+                        <input type="text"
+                            onChange={(event)=>{(this.handleInputText("professor",event))}}
+                            value={ this.props.professor }
+                        />
+                    </div>
                 }
                 <div className="course-row__hours">
                     Number of hours: { this.props.numberOfHours }
--- src/components/CoursesManagement/CoursesList/CoursesList.js
+++ src/components/CoursesManagement/CoursesList/CoursesList.js
@@ -1,3 +1,4 @@
+import produce from "immer";
 import React from "react";
 import CourseRow from "./CourseRow";
 
@@ -6,25 +7,68 @@ export default class CoursesList extends React.Component{
         super(props);
         this.state={
             courses: Array(1).fill({
-                color:"ffffff",
-                name: "Sin nombre",
-                abbreviation: "S. N.",
-                professor: null,
-                numberOfHours: 0
-            }),
-        }
-    }
-    handleClickOnShowProfessor(){
-        console.log("HOLA");
-        this.setState({
-            courses: Array(1).fill({
-                color:"ffffff",
+                color:"#F5BE29",
                 name: "Sin nombre",
                 abbreviation: "S. N.",
                 professor: "",
-                numberOfHours: 0
+                numberOfHours: 0,
+                hasProfessor: false,
             }),
+        }
+    }
+    handleClickOnShowProfessor(index){
+        const updatedCourses=produce(this.state.courses,(newCourses)=>{
+            newCourses[index]={
+                ...newCourses[index], hasProfessor: true
+            }
         })
+        this.setState({
+            courses: updatedCourses,
+        });
+    }
+    handleChangeColor(index, value){
+        const updatedCourses=produce(this.state.courses,(newCourses)=>{
+            newCourses[index]={
+                ...newCourses[index], color: value
+            }
+        })
+        this.setState({
+            courses: updatedCourses,
+        });
+        console.log(value);
+    }
+    handleChangeName(index, value){
+        const updatedCourses=produce(this.state.courses,(newCourses)=>{
+            newCourses[index]={
+                ...newCourses[index], name: value
+            }
+        })
+        this.setState({
+            courses: updatedCourses,
+        });
+        console.log(value);
+    }
+    handleChangeAbbreviation(index, value){
+        const updatedCourses=produce(this.state.courses,(newCourses)=>{
+            newCourses[index]={
+                ...newCourses[index], abbreviation: value
+            }
+        })
+        this.setState({
+            courses: updatedCourses,
+        });
+        console.log(value);
+    }
+    handleChangeProfessor(index, value){
+        const updatedCourses=produce(this.state.courses,(newCourses)=>{
+            newCourses[index]={
+                ...newCourses[index], professor: value
+            }
+        })
+        this.setState({
+            courses: updatedCourses,
+        });
+        console.log(value);
     }
     renderRows(){
 
@@ -32,7 +76,11 @@ export default class CoursesList extends React.Component{
             return <CourseRow
                 {...row}
                 key={index}
-                onClickOnShowProfessor={()=>this.handleClickOnShowProfessor()}
+                onClickOnShowProfessor={()=>this.handleClickOnShowProfessor(index)}
+                onChangeColor={(value)=>this.handleChangeColor(index, value)}
+                onChangeName={(value)=>this.handleChangeName(index, value)}
+                onChangeAbbreviation={(value)=>this.handleChangeAbbreviation(index, value)}
+                onChangeProfessor={(value)=>this.handleChangeProfessor(index, value)}
             />
         })
         return rows;
