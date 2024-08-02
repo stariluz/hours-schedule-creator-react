95aafb0f876300478d4dde2e1d5050cf022f9a3a
Adora <im.stariluz@gmail.com>
2024-08-01 22:34:52 -0600

    Fix #43 FlowType to adjust text constantly

--- src/components/Calendar/CalendarHourSpace/CalendarClassHour/CalendarClassHour.jsx
+++ src/components/Calendar/CalendarHourSpace/CalendarClassHour/CalendarClassHour.jsx
@@ -1,8 +1,12 @@
 import "../../../UI/Colors/Colors.css"
 import "./CalendarClassHour.css";
 import "../CalendarHourSpace.css";
+import { useRef } from "react";
+import FlowType from "../../../common/FlowType/FlowType";
 
 const CalendarClassHour = ({ time, content, firstHour }) => {
+  const $class = useRef(null);
+
   return (
     <button className="hour__class class-box custom-color custom-color-text"
       style={{
@@ -12,9 +16,13 @@ const CalendarClassHour = ({ time, content, firstHour }) => {
         "--hour-bg-saturation": `${content.color.s * 100}%`,
         "--hour-bg-lightness": `${content.color.l * 100}%`,
         "--input-color-text-color": content.text,
-      }}
+      }} ref={$class}
     >
-      {content.name}
+      <FlowType elementRef={$class} options={{
+        maxFont: 16, fontRatio: 10
+      }}>
+        {content.name}
+      </FlowType>
     </button>
   );
 }
