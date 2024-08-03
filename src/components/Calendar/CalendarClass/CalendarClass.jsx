da14d14b6dd0bddb7fcbad45c2b74adca6288071
Adora <im.stariluz@gmail.com>
2024-08-03 11:55:39 -0600

    Fix #43 Fix selection class grid and class padding

--- src/components/Calendar/CalendarClass/CalendarClass.jsx
+++ src/components/Calendar/CalendarClass/CalendarClass.jsx
@@ -5,26 +5,31 @@ import "../../UI/Colors/Colors.css"
 import "./CalendarClass.css";
 const CalendarClass = ({
   children, className = '', style = {},
+  onClick = () => { },
   course,
 }) => {
   const $class = useRef(null);
 
   return (
-    <button className={`${className} hour__class class-box custom-color custom-color-text`}
+    <button className={`${className} class-box custom-color custom-color-text`}
       style={{
         ...style,
         "--hour-bg-hue": `${course.color.h}`,
         "--hour-bg-saturation": `${course.color.s * 100}%`,
         "--hour-bg-lightness": `${course.color.l * 100}%`,
         "--input-color-text-color": course.text,
-      }} ref={$class}
+      }}
+
+      onClick={() => { onClick() }}
     >
       {children}
-      <FlowType elementRef={$class} options={{
-        maxFont: 24, fontRatio: 10
-      }}>
-        {course.name}
-      </FlowType>
+      <span className="content" ref={$class}>
+        <FlowType elementRef={$class} options={{
+          maxFont: 24, fontRatio: 10
+        }}>
+          {course.name}
+        </FlowType>
+      </span>
     </button >
   );
 }
